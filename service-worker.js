71ad76a8e5c50286f78e2ca018f4c1c41495bc92
SENSEI <inversionjean26@gmail.com>
2026-03-25 05:20:10 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,22 @@
 const CACHE_NAME = 'sensei-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './logo hackers.jpg',
+  './Musica/index.html',
+  './Musica/script.js',
+  './Musica/style.css',
+  './HEROIC Hard Epic String Rap Beat  Prod. By Aidan x Maxxton.mp3'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache abierta');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
@@ -21,5 +29,20 @@ self.addEventListener('fetch', event => {
         }
         return fetch(event.request);
       })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
