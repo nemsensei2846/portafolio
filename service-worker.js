654e34ca5451580d7245a6dc119ba997ebc899f6
SENSEI <inversionjean26@gmail.com>
2026-03-27 15:21:58 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sensei-v28'; // Versión 28 - Final Profile & Banner Logic Update
+const CACHE_NAME = 'sensei-v29'; // Versión 29 - Options Menu & Downloads Profile logic
 const urlsToCacheEssential = [
   './',
   './index.html',
